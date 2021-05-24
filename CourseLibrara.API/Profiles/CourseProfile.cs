2e83d6184ccc4535b91b361315324b1125e359f8
Davorin <davorin.jurkovic@gmail.com>
2021-05-25 00:06:31 +0200

    Profiles are added

---
+++ CourseLibrara.API/Profiles/CourseProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrara.API.Profiles
+{
+    public class CourseProfile : Profile
+    {
+        public CourseProfile()
+        {
+            CreateMap<Entities.Course, Models.CourseDto>();
+            CreateMap<Models.CourseForCreationDto, Entities.Course>();
+            CreateMap<Models.CourseForUpdateDto, Entities.Course>();
+            CreateMap<Entities.Course, Models.CourseForUpdateDto>();
+        }
+    }
+}
