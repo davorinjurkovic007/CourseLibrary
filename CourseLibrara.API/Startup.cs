6ad1540d86a971e67a0e221e9caeac15565bfe06
Davorin <davorin.jurkovic@gmail.com>
2021-05-24 18:01:01 +0200

    Setup database

--- CourseLibrara.API/Startup.cs
+++ CourseLibrara.API/Startup.cs
@@ -1,6 +1,8 @@
+using CourseLibrara.API.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,11 @@ namespace CourseLibrara.API
         {
 
             services.AddControllers();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CourseLibraryForTest"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
